3777d7134421ff31052692a6fe7c0be2f0dda24e
Guido Contreras Woda <guiwoda@gmail.com>
2016-09-10 18:39:46 -0300

    agrego test para método de ultima ciudad del trayecto

--- TrayectoTest.py
+++ TrayectoTest.py
@@ -6,14 +6,18 @@ from Trayecto import Trayecto
 
 class TrayectoTest(unittest.TestCase):
 
+    def setUp(self):
+        self.trayecto = Trayecto()
+
+        self.trayecto.rutas.append(Ruta('Buenos Aires', 'La Plata'))
+        self.trayecto.rutas.append(Ruta('La Plata', 'Bahia Blanca'))
+
+    def test_devuelve_la_ultima_ciudad(self):
+        self.assertEqual('Bahia Blanca', self.trayecto.ultima_ciudad())
+
     def test_encuentra_una_ciudad(self):
-        trayecto = Trayecto()
-
-        ruta = Ruta('Buenos Aires', 'La Plata')
-        trayecto.rutas.append(ruta)
-
-        self.assertTrue(trayecto.ciudad_existe('La Plata'))
-        self.assertFalse(trayecto.ciudad_existe('Cordoba'))
+        self.assertTrue(self.trayecto.ciudad_existe('La Plata'))
+        self.assertFalse(self.trayecto.ciudad_existe('Cordoba'))
 
 if __name__ == '__main__':
     unittest.main()

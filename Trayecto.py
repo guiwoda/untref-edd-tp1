e344b99ff78e2aea866cc428a5786b39a68fc7d8
Guido Contreras Woda <guiwoda@gmail.com>
2016-09-10 17:59:25 -0300

    El trayecto tiene un listado de rutas

--- Trayecto.py
+++ Trayecto.py
@@ -1,7 +1,25 @@
+from Ruta import *
+
+
 class Trayecto:
 
     def __init__(self):
-        self.ciudades = []
+        self.rutas = []
         self.distaciaTotal = 0
         self.tiempoTotal = 0
 
+    def ultima_ciudad(self):
+        return self.rutas[-1].destino
+
+    def ciudad_existe(self, ciudad):
+        return [ruta for ruta in self.rutas if ruta.origen == ciudad or ruta.destino == ciudad]
+
+if __name__ == '__main__':
+
+    trayecto = Trayecto()
+
+    ruta = Ruta('Buenos Aires', 'La Plata')
+    trayecto.rutas.append(ruta)
+
+    print('Encontre La Plata' if trayecto.ciudad_existe('La Plata') else '')
+    print('Encontre Cordoba' if trayecto.ciudad_existe('Cordoba') else '')

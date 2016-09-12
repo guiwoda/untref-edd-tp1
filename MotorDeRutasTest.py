b546da76c1a81ea6c0d71eb0ffdd28b2fa337e0b
Guido Contreras Woda <guiwoda@gmail.com>
2016-09-12 20:25:36 -0300

    Uso un cassette por caso de prueba

--- MotorDeRutasTest.py
+++ MotorDeRutasTest.py
@@ -14,18 +14,18 @@ class MotorDeRutasTest(unittest.TestCase):
         self.motor = MotorDeRutas()
 
     def test_crea_trayectos_nuevos(self):
-        with vcr.use_cassette('fixtures/ushuaia_la_quiaca.yaml'):
+        with vcr.use_cassette('fixtures/test_crea_trayectos_nuevos.yaml'):
             trayecto = self.motor.crear_trayecto('Ushuaia, Argentina', 'La Quiaca, Argentina', 'De Ushuaia a La Quiaca')
 
             self.assertIsInstance(trayecto, Trayecto)
 
     def test_falla_cuando_el_trayecto_no_existe(self):
-        with vcr.use_cassette('fixtures/bs_as_reykjavik.yaml'):
+        with vcr.use_cassette('fixtures/test_falla_cuando_el_trayecto_no_existe.yaml'):
             with self.assertRaises(RutaException):
                 self.motor.crear_trayecto('Buenos Aires, Argentina', 'Reykjavik, Islandia', 'islandia')
 
     def test_agrega_ciudad_a_trayecto(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata.yaml'):
+        with vcr.use_cassette('fixtures/test_agrega_ciudad_a_trayecto.yaml'):
             trayecto = self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'bs_as_la_plata')
             distancia = trayecto.distaciaTotal
             tiempo = trayecto.tiempoTotal
@@ -37,14 +37,14 @@ class MotorDeRutasTest(unittest.TestCase):
             self.assertGreater(trayecto.tiempoTotal, tiempo)
 
     def test_falla_cuando_la_ciudad_no_existe_en_trayecto(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata.yaml'):
+        with vcr.use_cassette('fixtures/test_falla_cuando_la_ciudad_no_existe_en_trayecto.yaml'):
             trayecto = self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'bs_as_la_plata')
 
             with self.assertRaises(TrayectoException):
                 self.motor.agregar_parada(trayecto.nombre, 'Tandil', 'Quilmes')
 
     def test_agrega_parada_a_trayecto(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_quilmes.yaml'):
+        with vcr.use_cassette('fixtures/test_agrega_parada_a_trayecto.yaml'):
             trayecto = self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'bs_as_la_plata')
             self.motor.agregar_ciudad(trayecto.nombre, 'Tandil')
 
@@ -57,7 +57,7 @@ class MotorDeRutasTest(unittest.TestCase):
             self.assertGreater(trayecto.distaciaTotal, distancia)
             self.assertGreater(trayecto.tiempoTotal, tiempo)
     def test_concatena_trayectos(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_quilmes_tandil.yaml'):
+        with vcr.use_cassette('fixtures/test_concatena_trayectos.yaml'):
             trayectoInicial = self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'trayecto_inicial')
             trayectoFinal = self.motor.crear_trayecto('Quilmes', 'Tandil', 'trayecto_final')
             self.motor.concatenar(trayectoInicial.nombre, trayectoFinal.nombre)
@@ -65,23 +65,23 @@ class MotorDeRutasTest(unittest.TestCase):
             self.assertEqual(['Buenos Aires', 'La Plata', 'Quilmes', 'Tandil'], trayectoInicial.obtener_ciudades())
 
     def test_obtiene_trayectos_por_nombre(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata.yaml'):
+        with vcr.use_cassette('fixtures/test_obtiene_trayectos_por_nombre.yaml'):
             trayecto = self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'bs_as_la_plata')
 
             self.assertEqual(trayecto, self.motor.obtener_trayecto(trayecto.nombre))
 
     def test_obtiene_rutas_entre_dos_ciudades(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata.yaml'):
+        with vcr.use_cassette('fixtures/test_obtiene_rutas_entre_dos_ciudades.yaml'):
             ruta = self.motor.obtener_ruta('Buenos Aires', 'La Plata')
 
             self.assertIsInstance(ruta, Ruta)
     def test_falla_al_obtener_rutas_entre_dos_ciudades_desconectadas(self):
-        with vcr.use_cassette('fixtures/bs_as_reykjavik.yaml'):
+        with vcr.use_cassette('fixtures/test_falla_al_obtener_rutas_entre_dos_ciudades_desconectadas.yaml'):
             with self.assertRaises(RutaException):
                 self.motor.obtener_ruta('Buenos Aires, Argentina', 'Reykjavik, Islandia')
 
     def test_muestra_trayectos(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata.yaml'):
+        with vcr.use_cassette('fixtures/test_muestra_trayectos.yaml'):
             self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'bs_as_la_plata')
 
             self.assertEqual(
@@ -91,7 +91,7 @@ tiempo estimado de viaje: 3366''',
                 self.motor.mostrar('bs_as_la_plata'))
 
     def test_compara_trayectos_por_distancia(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_quilmes_tandil.yaml'):
+        with vcr.use_cassette('fixtures/test_compara_trayectos_por_distancia.yaml'):
             buenos_aires_la_plata = self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'buenos_aires_la_plata')
             quilmes_tandil        = self.motor.crear_trayecto('Quilmes',      'Tandil',   'quilmes_tandil')
 
@@ -100,7 +100,7 @@ tiempo estimado de viaje: 3366''',
             self.assertEqual(1,  self.motor.comparar(quilmes_tandil.nombre,        buenos_aires_la_plata.nombre, 'd'))
 
     def test_compara_trayectos_por_tiempo(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_quilmes_tandil.yaml'):
+        with vcr.use_cassette('fixtures/test_compara_trayectos_por_tiempo.yaml'):
             buenos_aires_la_plata = self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'buenos_aires_la_plata')
             quilmes_tandil        = self.motor.crear_trayecto('Quilmes',      'Tandil',   'quilmes_tandil')
 
@@ -109,7 +109,7 @@ tiempo estimado de viaje: 3366''',
             self.assertEqual(1,  self.motor.comparar(quilmes_tandil.nombre,        buenos_aires_la_plata.nombre, 't'))
 
     def test_muestra_rutas_de_un_trayecto(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_la_quiaca.yaml'):
+        with vcr.use_cassette('fixtures/test_muestra_rutas_de_un_trayecto.yaml'):
             trayecto = self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'bs_as_la_plata')
             self.motor.agregar_ciudad(trayecto.nombre, 'La Quiaca')
 
@@ -127,14 +127,14 @@ La Plata - La Quiaca
             )
 
     def test_muestra_trayectos_disponibles(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_quilmes_tandil.yaml'):
+        with vcr.use_cassette('fixtures/test_muestra_trayectos_disponibles.yaml'):
             self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'tray_a')
             self.motor.crear_trayecto('Quilmes', 'Tandil', 'tray_b')
 
             self.assertEqual({'tray_a', 'tray_b'}, set(self.motor.listar()))
 
     def test_persiste_los_trayectos(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_quilmes_tandil.yaml'):
+        with vcr.use_cassette('fixtures/test_persiste_los_trayectos.yaml'):
             self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'tray_a')
             self.motor.crear_trayecto('Quilmes', 'Tandil', 'tray_b')
 
@@ -146,13 +146,13 @@ La Plata - La Quiaca
             self.assertEqual({'tray_a', 'tray_b'}, set(self.motor.listar()))
 
     def test_sabe_si_hay_cambios_sin_guardar(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_quilmes_tandil.yaml'):
+        with vcr.use_cassette('fixtures/test_sabe_si_hay_cambios_sin_guardar.yaml'):
             self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'algo_nuevo')
 
             self.assertFalse(self.motor.esta_guardado())
 
     def test_sabe_si_hay_cambios_guardados(self):
-        with vcr.use_cassette('fixtures/bs_as_la_plata_quilmes_tandil.yaml'):
+        with vcr.use_cassette('fixtures/test_sabe_si_hay_cambios_guardados.yaml'):
             self.motor.crear_trayecto('Buenos Aires', 'La Plata', 'tray_a')
             self.motor.guardar('tray_a')
 
